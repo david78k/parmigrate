cd37022f86badad16bea07f767668ae46b845ffa
david78k <root@gr120.hpcc.jp>
2013-09-26 08:44:38 +0900

    genplot

--- log/old/aggregate/20-80-180/genplot.m
+++ log/old/aggregate/20-80-180/genplot.m
@@ -15,8 +15,8 @@ figure
 %gplot rand (100,1) with linespoints
 x = -10:0.1:10;
 %plot (x, sin (x));
-plot(A(:,:))
-%plot(A(:,1:2))
+%plot(A(:,:))
+plot(A(:,1:2))
 %plot(vwnds)
 %plot(RTTs, Ns)
 %xlabel('time')
