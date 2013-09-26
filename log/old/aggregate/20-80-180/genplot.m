5f72f4aa56a2a02daf40ccb8adb0090dcdacd757
david78k <root@gr120.hpcc.jp>
2013-09-26 10:58:08 +0900

    genplot

--- log/old/aggregate/20-80-180/genplot.m
+++ log/old/aggregate/20-80-180/genplot.m
@@ -17,7 +17,7 @@ figure
 x = -10:0.1:10;
 %plot (x, sin (x));
 %plot(A(:,:))
-plot(A(:,[1, 5]))
+plot(A(:,[1, 5]));
 %plot(A(:,1:5))
 %plot(RTTs, Ns)
 xlabel('ITERATION')
@@ -33,3 +33,5 @@ figure
 output = strcat(prefix, ".bw")
 plot(A(:,2:3))
 saveas (1, strcat(output, ".png"));
+xlabel('ITERATION')
+ylabel('VM WINDOW')
